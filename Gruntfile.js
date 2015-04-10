cbb8af0d48174ca2131e7f5e66b2bae56f9166cb
Camille Roussel <camille@rousselfamily.com>
2015-04-10 10:36:49 -0700

    (feat) Flattened node_modules and handled __dirname issues for azure environment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 'use strict';
 module.exports = function(grunt) {
+  grunt.file.setBase(__dirname);
   grunt.loadNpmTasks('grunt-notify');
   grunt.loadNpmTasks('grunt-jsxhint');
   grunt.loadNpmTasks('grunt-contrib-watch');
