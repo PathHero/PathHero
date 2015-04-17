48bc5501c61bec32aa04d178b09fcc60ba83589e
Ben Rodigas <benjamin.rodigas@gmail.com>
2015-04-17 16:38:05 -0700

    nav first draft

--- Client/play/src/BottomNav.jsx
+++ Client/play/src/BottomNav.jsx
@@ -3,23 +3,36 @@
 
 var React = require('react');
 var Link = require('react-router').Link;
+var $ = require('jquery');
 
 module.exports = React.createClass({
+  
   render: function () {
     return (                   
       <div id="bottomNav">
-        <div className="row">
-          <div className="col-xs-4">
-            <span><Link to="status">Status</Link></span>
-          </div>
-          <div className="col-xs-4">
-            <span><Link to="clues">Clues</Link></span>
-          </div>
-          <div className="col-xs-4">
-            <span><Link to="map">Map</Link></span>
+        <div className="parent2">
+            <div className="test1">
+              <Link to="status">
+              <i className="fa fa-list-ul"></i>
+              STATUS</Link>            
+            </div>
+            <div className="test2">
+              <Link to="clues">
+                <i className="fa fa-question"></i>
+                CLUES  
+              </Link>
+            </div>
+            <div className="test3">
+              <Link to="map">
+                <i className="fa fa-map-marker"></i>
+                MAP
+              </Link>
+            </div>
+          <div className="mask">
+            <i className="fa fa-home fa-2x"></i>
           </div>
         </div>
-      </div>      
+      </div> 
     );
   }
 });
