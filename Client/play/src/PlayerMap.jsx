3a66d6afba66a60a14fd1b133d0db34e2395d3df
Camille Roussel <camille@rousselfamily.com>
2015-04-17 21:31:02 -0700

    (refactor) Status progress bar. close #113

--- Client/play/src/PlayerMap.jsx
+++ Client/play/src/PlayerMap.jsx
@@ -23,7 +23,10 @@ module.exports = React.createClass({
     gMap.getGeolocation(function(value){
       gMap.setCenter(value);
       var mapArray = [];
-      for (var i = 0; i < hunt.pins.length && i < hunt.currentPinIndex; i++) {
+      var currentPinIndex = hunt.currentPinIndex;
+      var numOfPins = hunt.pins.length;
+      currentPinIndex = Math.min(currentPinIndex, numOfPins);
+      for (var i = 0; i < hunt.pins.length && i < currentPinIndex; i++) {
         mapArray.push([hunt.pins[i].geo.lat,hunt.pins[i].geo.lng]);
       }
       if(mapArray.length > 0){
