1d91365e99d310c99fb28598e9ed332c8630c74e
Camille Roussel <camille@rousselfamily.com>
2015-04-24 11:34:55 -0700

    (refactor) added cue to click on map, closes #232

--- Client/create/src/PinList.jsx
+++ Client/create/src/PinList.jsx
@@ -6,12 +6,17 @@ var Pin = require('./Pin');
 
 module.exports = React.createClass({
   render: function() {
-    var pinNodes = this.props.pins.map(function(pin, index) {
-      return (
-        <Pin pinIndex={index} pin={pin} key={index} editMode={this.props.editMode}>
-        </Pin>
-      );
-    }, this);
+    var pinNodes;
+    if (this.props.pins.length === 0) {
+      pinNodes = (<h2>Click on the map to add locations.</h2>);
+    } else {
+      pinNodes = this.props.pins.map(function(pin, index) {
+        return (
+          <Pin pinIndex={index} pin={pin} key={index} editMode={this.props.editMode}>
+          </Pin>
+        );
+      }, this);
+    }
     return (
       <div className="pinList">
         {pinNodes}
